acd832c22659361cf90d970bb93399ed65a9ca6a
Swapnil Subhash Pawar <127788263+Ragnar-4912@users.noreply.github.com>
2024-03-17 16:57:58 +0100

    Update Cross_Dot-Game.py

--- Cross_Dot-Game.py
+++ Cross_Dot-Game.py
@@ -1,3 +1,13 @@
+###
+Here are the requirements:
+
+2 players should be able to play the game (both sitting at the same computer)
+The board should be printed out every time a player makes a move
+You should be able to accept input of the player position and then place a symbol on the board
+
+###
+
+
 def user_in(board,value,mark):
     
     while int(value) not in check_choice :
